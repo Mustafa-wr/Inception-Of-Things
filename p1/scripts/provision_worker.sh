5f3ef8bf855cd6971f6ee8afa8014ae44046048c
Mustafa-wr <mradwan@student.42abudhabi.ae>
2024-07-04 07:28:41 +0400

    net-tool fix

--- p1/scripts/provision_worker.sh
+++ p1/scripts/provision_worker.sh
@@ -7,3 +7,4 @@ K3S_TOKEN="/vagrant/confs/server_token.txt"
 curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN_FILE=${K3S_TOKEN}  INSTALL_K3S_EXEC="--flannel-iface=eth1" sh -
 sudo rm -rf /vagrant/confs/*
 sudo apt install -y net-tools
+export PATH=$PATH:/sbin:/usr/sbin
