3637585b0a3589791834d6b061a9607d1766a6c8
Mustafa-wr <mradwan@student.42abudhabi.ae>
2024-10-30 15:51:15 +0400

    small changes has been added to p1

--- p1/scripts/provision_server.sh
+++ p1/scripts/provision_server.sh
@@ -3,10 +3,11 @@
 sudo apt-get update
 sudo apt-get install -y curl
 sudo touch /vagrant/confs/server_token.txt
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--bind-address=192.168.56.110 --flannel-iface=eth1" sh -
+
+# Set Flannel (network overlay for cross-node pod communication) to use eth1 in VM setup
+curl -L https://get.k3s.io | INSTALL_K3S_EXEC="--bind-address=192.168.56.110 --flannel-iface=eth1" sh - 
+
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/confs/server_token.txt
-sudo cp /etc/rancher/k3s/k3s.yaml /vagrant/confs/
-sudo apt install -y net-tools
+sudo cp /etc/rancher/k3s/k3s.yaml /vagrant/confs/ # Copy kubeconfig file to shared folder
+sudo apt install -y net-tools # Install net-tools for ifconfig
 export PATH=$PATH:/sbin:/usr/sbin
-
-
