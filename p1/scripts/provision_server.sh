8de51b9f91c79a4dc88558ee99d7ecb1e254dfc1
Mustafa-wr <mradwan@student.42abudhabi.ae>
2024-11-07 12:28:03 +0400

    Three apps successfully worked with vagrant

--- p1/scripts/provision_server.sh
+++ p1/scripts/provision_server.sh
@@ -7,6 +7,8 @@ sudo touch /vagrant/confs/server_token.txt
 # Set Flannel (network overlay for cross-node pod communication) to use eth1 in VM setup
 curl -L https://get.k3s.io | INSTALL_K3S_EXEC="--bind-address=192.168.56.110 --flannel-iface=eth1" sh - 
 
+sudo apt install -y vim
+
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/confs/server_token.txt
 sudo cp /etc/rancher/k3s/k3s.yaml /vagrant/confs/ # Copy kubeconfig file to shared folder
 sudo apt install -y net-tools # Install net-tools for ifconfig
