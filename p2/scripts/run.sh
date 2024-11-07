0df46635990f9540999d361bbb7df1811ff1502f
Mustafa-wr <mradwan@student.42abudhabi.ae>
2024-11-07 12:26:36 +0400

    Three apps successfully worked with vagrant

---
+++ p2/scripts/run.sh
@@ -0,0 +1,6 @@
+sudo apt upgrade -y && sudo apt update -y
+sudo apt install -y curl
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--bind-address=192.168.56.110 --flannel-iface=eth1" sh -
+cp /vagrant/scripts/three_app.yaml /vagrant/
+sudo kubectl apply -f /vagrant/three_app.yaml
+sudo apt install -y net-tools 
